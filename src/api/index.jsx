8ae84be431ba85fd72d268b1947fe3cd9a356010
Mykhailo <mixa450@gmail.com>
2021-04-11 19:31:27 +0300

    v.1.0.1

---
+++ src/api/index.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+    let changeableUrl = url;
+
+    if (country) {
+        changeableUrl = `${url}/countries/${country}`
+    }
+
+    try {
+        const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(changeableUrl);
+
+        const modifyData = {
+            confirmed,
+            recovered,
+            deaths,
+            lastUpdate
+        };
+
+        return modifyData;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const fetchDailyDate = async () => {
+    try {
+        const {data} = await axios.get(`${url}/daily`);
+        const modifyDate = data.map(dailyData => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }))
+        return modifyDate;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const fetchCountries = async () =>{
+    try {
+        const {data:{countries}} = await axios.get(`${url}/countries`);
+        return countries.map( country => country.name);
+    } catch (e) {
+        console.log(e);
+    }
+}
