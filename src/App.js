2b31f88e964827b12d9efef21f11f15be8827826
Mykhailo <mixa450@gmail.com>
2021-04-12 11:11:46 +0300

    v1.0.3 (eslint fix)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 import React from 'react';
-import {Cards, Chart, CountryPicker} from './components';
+import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
-import {fetchData} from './api'
+import { fetchData } from './api';
 import covidPNG from './images/covid19_logo.png';
 
-
 class App extends React.Component {
+     state = {
+       data: {},
+       country: '',
+     }
 
-    state = {
-        data: {},
-        country: ''
-    }
+     async componentDidMount() {
+       const fetchedData = await fetchData();
+       this.setState({ data: fetchedData });
+     }
 
-    async componentDidMount() {
-        const fetchedData = await fetchData();
-        this.setState({data: fetchedData})
-    }
-
-    handleCountryChange = async  (country) => {
-
-        const fetchedData = await fetchData(country)
-        this.setState({data: fetchedData, country: country});
+    handleCountryChange = async (country) => {
+      const fetchedData = await fetchData(country);
+      this.setState({ data: fetchedData, country });
     }
 
     render() {
-        const {data, country} = this.state;
-        return (
-            <div className={styles.container}>
-                <img  className={styles.image} src={covidPNG} alt={'covid-19'}/>
-                <Cards data={data}/>
-                <CountryPicker handleCountryChange={this.handleCountryChange}/>
-                <Chart data={data} country={country}/>
-            </div>
-        );
+      const { data, country } = this.state;
+      return (
+        <div className={styles.container}>
+          <img className={styles.image} src={covidPNG} alt="covid-19" />
+          <Cards data={data} />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} />
+        </div>
+      );
     }
-
-
 }
 
 export default App;
--- src/api/index.jsx
+++ src/api/index.jsx
@@ -3,47 +3,51 @@ import axios from 'axios';
 const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async (country) => {
-    let changeableUrl = url;
+  let changeableUrl = url;
 
-    if (country) {
-        changeableUrl = `${url}/countries/${country}`
-    }
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
 
-    try {
-        const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(changeableUrl);
+  try {
+    const {
+      data: {
+        confirmed, recovered, deaths, lastUpdate,
+      },
+    } = await axios.get(changeableUrl);
 
-        const modifyData = {
-            confirmed,
-            recovered,
-            deaths,
-            lastUpdate
-        };
+    const modifyData = {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
 
-        return modifyData;
-    } catch (e) {
-        console.log(e);
-    }
-}
+    return modifyData;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const fetchDailyDate = async () => {
-    try {
-        const {data} = await axios.get(`${url}/daily`);
-        const modifyDate = data.map(dailyData => ({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            date: dailyData.reportDate
-        }))
-        return modifyDate;
-    } catch (e) {
-        console.log(e);
-    }
-}
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const modifyDate = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+    return modifyDate;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-export const fetchCountries = async () =>{
-    try {
-        const {data:{countries}} = await axios.get(`${url}/countries`);
-        return countries.map( country => country.name);
-    } catch (e) {
-        console.log(e);
-    }
-}
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
+  } catch (e) {
+    console.log(e);
+  }
+};
